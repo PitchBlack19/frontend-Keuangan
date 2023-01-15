09c6105fe6a3354dcabfe93241275471f097efc2
PitchBlack19 <firmanahmadmaulana819@gmail.com>
2023-01-15 19:55:12 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script>
+
+
+export default{
+  data(){
+    return{
+      datasensor :"Belum ada"
+    }
+  },
+
+  created(){
+    this.sambutan()
+  },
+  
+  methods:{
+    sambutan(){
+      alert("selamat datang");
+    },
+    async updata(){
+      const data = await fetch("http://127.0.0.1:82/getdata")
+      const datanya1 = await data.json()
+      this.datasensor = datanya1.postData
+    }
+  }
+}
+//tempat ngoding javascript
+</script>
+
+<template>
+<!----tempat ngoding html-->
+  <div>
+    Laporan keuangan {{ datasensor }}
+    <button @click="updata">dapatkan</button>
+  </div>
+  
+</template>
